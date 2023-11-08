4e40f161a74c6cef7114cd60f32557b0e2c8acd9
SO HUI <haejuuun925@gmail.com>
2023-11-09 01:39:55 +0900

    feat: dummy movies list 추가 및 Moive, MoviesList 컴포넌트 구현 & 스타일링

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
+import MoviesList from './components/MoviesList';
 
 function App() {
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
   return (
-    <div>
-      
-    </div>
+    <Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </Fragment>
   );
 }
 
